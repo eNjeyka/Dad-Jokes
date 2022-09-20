e5ffcbc1b9a61a801f9ed9201410a4949867f92f
Max Jey <enjeyka@yahoo.com>
2022-09-20 16:54:32 +0300

    added js logic and api

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+let jokeEl = document.getElementById("joke");
+let jokeBtn = document.getElementById("new-joke");
 
+jokeBtn.addEventListener('click', generateJoke)
+generateJoke();
+
+async function generateJoke() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos")
+
+    const data = await res.json()
+    
+    let num = Math.ceil(Math.random() * 100)
+    jokeEl.innerHTML = data[num].title
+}
+
+// function generateJoke() {
+//     const config = {
+//         mode: "no-cors",
+//         headers: {
+//             Accept: "application/json",
+//         },
+//     };
+//     fetch("https://jsonplaceholder.typicode.com/todos")
+//         .then((res) => res.json())
+//         .then((data) => {
+//             let num = Math.random() * 100
+//             num = Math.ceil(num)
+//             jokeEl.innerHTML = data[num].title
+//         });
+// }
